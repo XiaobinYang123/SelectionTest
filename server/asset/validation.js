4c0f20b4f05971813336d4294387e15c65697100
q632346824 <632346824@qq.com>
2022-02-27 11:51:22 -0800

    build second endpoint and test

---
+++ server/asset/validation.js
@@ -0,0 +1,22 @@
+const { check } = require("express-validator");
+
+const addSubmissionValidate = [
+  check("location", "location is required").notEmpty(),
+  check("quantity", "quantity is required")
+    .isNumeric()
+    .withMessage("quantity must be numeric")
+    .isInt({ min: 1 })
+    .withMessage("ducks must be greater than 0"),
+  check("foodName", "food name is required").notEmpty(),
+  check("foodWeight", "food weight is required")
+    .isNumeric()
+    .withMessage("Food weight must be numeric")
+    .isInt({ gt: 0 })
+    .withMessage("Food weight must be greater than 0"),
+  check("date", "Date is required")
+    .notEmpty()
+    .isISO8601()
+    .withMessage("Date should be valid"),
+];
+
+exports.addSubmissionValidate = addSubmissionValidate;
--- server/routes/submissions.js
+++ server/routes/submissions.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const { validationResult } = require("express-validator");
+
 const Submission = require("../models/submissions");
+const { addSubmissionValidate } = require("../asset/validation");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -9,6 +12,28 @@ router.get("/", async (req, res, next) => {
     res.json(submissions);
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/add", addSubmissionValidate, async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+
+  const submission = new Submission({
+    location: req.body.location,
+    quantity: req.body.quantity,
+    foodName: req.body.foodName,
+    foodWeight: req.body.foodWeight,
+    date: req.body.date,
+  });
+
+  try {
+    const response = await submission.save();
+    res.status(201).json(response);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
